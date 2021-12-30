f9962a44da5805c6866bd9f3b4f5fdc9c47f7bd0
Dave Horton <dhorton668@outlook.com>
2021-12-30 11:25:07 -0600

    added compose project

--- run.sh
+++ run.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-docker run -d \
-  --name container-central \
-  -p 8088:8088 \
-  -v /var/run/docker.sock:/var/run/docker.sock \
-  davescodemusings/container-central:latest
+docker-compose -f compose/container-central.yml -p container-central up -d
